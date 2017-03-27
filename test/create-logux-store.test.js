2475a08e2bdab4be8667b2a6e139df31af0c58df
Andrey Sitnik <andrey@sitnik.ru>
2017-03-27 23:31:22 +0100

    Replace store dispatch by hack

--- test/create-logux-store.test.js
+++ test/create-logux-store.test.js
@@ -8,21 +8,32 @@ function reducer (state, action) {
   }
 }
 
-it('creates Redux store', function () {
-  var store = createLoguxStore(reducer, { value: 0 }, undefined, {
+function createStore () {
+  return createLoguxStore(reducer, { value: 0 }, undefined, {
     subprotocol: '1.0.0',
     userId: 10,
     url: 'wss://localhost:1337'
   })
+}
+
+it('creates Redux store', function () {
+  var store = createStore()
   store.dispatch({ type: 'INC' })
   expect(store.getState()).toEqual({ value: 1 })
 })
 
 it('creates Logux client', function () {
-  var store = createLoguxStore(reducer, { value: 0 }, undefined, {
-    subprotocol: '1.0.0',
-    userId: 10,
-    url: 'wss://localhost:1337'
-  })
+  var store = createStore()
   expect(store.client.options.subprotocol).toEqual('1.0.0')
+})
+
+it('sets tab ID', function () {
+  var store = createStore()
+  return new Promise(function (resolve) {
+    store.client.log.on('add', function (action, meta) {
+      expect(meta.tab).toEqual(store.client.id)
+      resolve()
+    })
+    store.dispatch({ type: 'INC' })
+  })
 })
