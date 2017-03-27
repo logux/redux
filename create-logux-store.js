8311b0908d233ae46539eb765b559c0bb113c59d
Andrey Sitnik <andrey@sitnik.ru>
2017-03-27 18:35:23 +0100

    Create Logux client

--- create-logux-store.js
+++ create-logux-store.js
@@ -1,6 +1,8 @@
 var createStore = require('redux').createStore
+var Client = require('logux-client/client')
 
-module.exports = function (reducer, preloadedState, enhancer) {
+module.exports = function (reducer, preloadedState, enhancer, config) {
   var store = createStore(reducer, preloadedState, enhancer)
+  store.client = new Client(config)
   return store
 }
--- test/create-logux-store.test.js
+++ test/create-logux-store.test.js
@@ -9,7 +9,20 @@ function reducer (state, action) {
 }
 
 it('creates Redux store', function () {
-  var store = createLoguxStore(reducer, { value: 0 })
+  var store = createLoguxStore(reducer, { value: 0 }, undefined, {
+    subprotocol: '1.0.0',
+    userId: 10,
+    url: 'wss://localhost:1337'
+  })
   store.dispatch({ type: 'INC' })
   expect(store.getState()).toEqual({ value: 1 })
+})
+
+it('creates Logux client', function () {
+  var store = createLoguxStore(reducer, { value: 0 }, undefined, {
+    subprotocol: '1.0.0',
+    userId: 10,
+    url: 'wss://localhost:1337'
+  })
+  expect(store.client.options.subprotocol).toEqual('1.0.0')
 })
