1cb2b78f26f3db91546ed9cf4c5db5ed2a826939
Andrey Sitnik <andrey@sitnik.ru>
2017-03-27 18:44:07 +0100

    Add base JSDoc

--- create-logux-store.js
+++ create-logux-store.js
@@ -1,8 +1,20 @@
 var createStore = require('redux').createStore
 var Client = require('logux-client/client')
 
-module.exports = function (reducer, preloadedState, enhancer, config) {
+/**
+ * Creates Logux client and connect it to Redux store.
+ *
+ * @param {function} reducer Redux reducer.
+ * @param {any} preloadedState Initial Redux state.
+ * @param {function} enhancer Redux middleware.
+ * @param {object} config Logux Client config.
+ *
+ * @return {object} Redux store with Logux hacks.
+ */
+function createLoguxStore (reducer, preloadedState, enhancer, config) {
   var store = createStore(reducer, preloadedState, enhancer)
   store.client = new Client(config)
   return store
 }
+
+module.exports = createLoguxStore
