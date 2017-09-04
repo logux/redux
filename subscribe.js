b9844c330eab0a5f8165fa4f00f7f87ea0d67bb5
Andrey Sitnik <andrey@sitnik.ru>
2017-09-04 16:10:19 +0800

    Add subscribe decorator

---
+++ subscribe.js
@@ -0,0 +1,115 @@
+var React = require('react')
+
+function merge (to, from) {
+  for (var i in from) to[i] = from[i]
+  return to
+}
+
+/**
+ * Decorator to add subscribe action on component mount and unsubscribe
+ * on unmount.
+ *
+ * @param {subscriber} subscriber Callback to return subscribe action
+ *                                properties according to component props.
+ * @param {object} [options] Redux options.
+ * @param {string} [options.storeKey] The store key name in context.
+ *
+ * @return {function} Class wrapper.
+ *
+ * @example
+ * const subscribe = require('react-logux/subscribe')
+ * @subscribe(({ id }) => `user/${ id }')
+ * class User extends React.Component { … }
+ *
+ * @example
+ * const subscribe = require('react-logux/subscribe')
+ * class User extends React.Component { … }
+ * const SubscribeUser = subscribe(props => {
+ *   return { name: `user/${ props.id }`, fields: ['name'] }
+ * })(User)
+ */
+function subscribe (subscriber, options) {
+  var storeKey = 'store'
+  if (options && options.storeKey) storeKey = options.storeKey
+
+  return function (Wrapped) {
+    var wrappedName = Wrapped.displayName || Wrapped.name || 'Component'
+
+    function SubscribeComponent () {
+      React.Component.apply(this, arguments)
+    }
+
+    SubscribeComponent.displayName = 'Subscribe' + wrappedName
+
+    SubscribeComponent.contextTypes = { }
+    SubscribeComponent.contextTypes[storeKey] = function () { }
+
+    SubscribeComponent.prototype = Object.create(React.Component.prototype, {
+      constructor: SubscribeComponent
+    })
+    Object.setPrototypeOf(SubscribeComponent, React.Component)
+
+    SubscribeComponent.prototype.componentWillMount = function () {
+      this.subscribe(this.props)
+    }
+
+    SubscribeComponent.prototype.componentWillReceiveProps = function (props) {
+      this.unsubscribe()
+      this.subscribe(props)
+    }
+
+    SubscribeComponent.prototype.componentWillUnmount = function () {
+      this.unsubscribe()
+    }
+
+    SubscribeComponent.prototype.subscribe = function (props) {
+      this.properties = subscriber(props)
+      if (typeof this.properties === 'string') {
+        this.properties = { name: this.properties }
+      }
+      this.json = JSON.stringify(props)
+
+      var store = this.context[storeKey]
+
+      if (!store.subscribers) store.subscribers = { }
+      if (!store.subscribers[this.json]) store.subscribers[this.json] = 0
+
+      if (store.subscribers[this.json] === 0) {
+        var action = merge({ type: 'logux/subscribe' }, this.properties)
+        store.log.add(action, { sync: true })
+      }
+      store.subscribers[this.json] += 1
+    }
+
+    SubscribeComponent.prototype.unsubscribe = function () {
+      var store = this.context[storeKey]
+
+      store.subscribers[this.json] -= 1
+
+      if (store.subscribers[this.json] === 0) {
+        var action = merge({ type: 'logux/unsubscribe' }, this.properties)
+        store.log.add(action, { sync: true })
+      }
+    }
+
+    SubscribeComponent.prototype.render = function () {
+      return React.createElement(Wrapped, this.props)
+    }
+
+    return SubscribeComponent
+  }
+}
+
+module.exports = subscribe
+
+/**
+ * @callback subscriber
+ * @param {object} props The component properties.
+ * @return {string|Subscription} The subscription action properties.
+ */
+
+/**
+ * Details for subscription action.
+ * @typedef {object} Subscription
+ * @property {string} name The subscription name.
+ */
---
+++ test/subscribe.test.js
@@ -0,0 +1,201 @@
+var createLoguxCreator = require('logux-redux/create-logux-creator')
+var createReactClass = require('create-react-class')
+var Provider = require('react-redux').Provider
+var renderer = require('react-test-renderer')
+var React = require('react')
+
+var subscribe = require('../subscribe')
+
+var h = React.createElement
+
+function createComponent (content) {
+  var createStore = createLoguxCreator({
+    subprotocol: '0.0.0',
+    server: 'wss://localhost:1337',
+    userId: false
+  })
+  var store = createStore(function () {
+    return { }
+  })
+  var component = renderer.create(h(Provider, { store: store }, content))
+  component.client = store.client
+  return component
+}
+
+function UserPhoto (props) {
+  return h('img', { src: props.id + '.jpg' })
+}
+var SubscribeUserPhoto = subscribe(function (props) {
+  return { name: 'users/' + props.id, fields: ['photo'] }
+})(UserPhoto)
+
+it('generates component name', function () {
+  var User5 = createReactClass({
+    displayName: 'User5',
+    render: function () { }
+  })
+  var SubscribeUser5 = subscribe(function () {
+    return 'users/10'
+  })(User5)
+  expect(SubscribeUser5.displayName).toEqual('SubscribeUser5')
+
+  // eslint-disable-next-line es5/no-classes
+  class User6 extends React.Component {
+    render () { }
+  }
+  var SubscribeUser6 = subscribe(function () {
+    return 'users/10'
+  })(User6)
+  expect(SubscribeUser6.displayName).toEqual('SubscribeUser6')
+
+  var SubscribeNameless = subscribe(function () {
+    return 'users/10'
+  })(function () { })
+  expect(SubscribeNameless.displayName).toEqual('SubscribeComponent')
+})
+
+it('passes properties', function () {
+  var Post = createReactClass({
+    render: function () {
+      return h('article', { },
+        h('h1', { }, this.props.title),
+        this.props.children)
+    }
+  })
+  var SubscribePost = subscribe(function () {
+    return 'posts/10'
+  })(Post)
+
+  var component = createComponent(
+    h(SubscribePost, { title: 'A' }, h('p', { }, 'Text'))
+  )
+  expect(component.toJSON()).toEqual({
+    type: 'article',
+    props: { },
+    children: [
+      { type: 'h1', props: { }, children: ['A'] },
+      { type: 'p', props: { }, children: ['Text'] }
+    ]
+  })
+})
+
+it('subscribes', function () {
+  function User () {
+    return null
+  }
+  var SubscribeUser = subscribe(function (props) {
+    return 'users/' + props.id
+  })(User)
+
+  var component = createComponent(h('div', { }, [
+    h(SubscribeUser, { id: '1', key: 1 }),
+    h(SubscribeUser, { id: '1', key: 2 }),
+    h(SubscribeUser, { id: '2', key: 3 })
+  ]))
+  expect(component.client.subscriptions).toEqual([
+    { type: 'logux/subscribe', name: 'users/1' },
+    { type: 'logux/subscribe', name: 'users/2' }
+  ])
+})
+
+it('unsubscribes', function () {
+  var UserList = createReactClass({
+    getInitialState: function () {
+      return {
+        users: { a: 1, b: 1, c: 2 }
+      }
+    },
+    change: function (users) {
+      this.setState({ users: users })
+    },
+    render: function () {
+      var users = this.state.users
+      return h('div', {
+        onClick: this.change
+      }, Object.keys(this.state.users).map(function (key) {
+        return h(SubscribeUserPhoto, { id: users[key], key: key })
+      }))
+    }
+  })
+
+  var component = createComponent(h(UserList, { }))
+  expect(component.client.subscriptions).toEqual([
+    { type: 'logux/subscribe', name: 'users/1', fields: ['photo'] },
+    { type: 'logux/subscribe', name: 'users/2', fields: ['photo'] }
+  ])
+
+  component.toJSON().props.onClick([1, 2])
+  expect(component.client.subscriptions).toEqual([
+    { type: 'logux/subscribe', name: 'users/1', fields: ['photo'] },
+    { type: 'logux/subscribe', name: 'users/2', fields: ['photo'] }
+  ])
+
+  component.toJSON().props.onClick({ a: 1 })
+  expect(component.client.subscriptions).toEqual([
+    { type: 'logux/subscribe', name: 'users/1', fields: ['photo'] }
+  ])
+})
+
+it('changes subscription', function () {
+  var Profile = createReactClass({
+    getInitialState: function () {
+      return {
+        id: 1
+      }
+    },
+    change: function (id) {
+      this.setState({ id: id })
+    },
+    render: function () {
+      return h('div', { onClick: this.change },
+        h(SubscribeUserPhoto, { id: this.state.id })
+      )
+    }
+  })
+
+  var component = createComponent(h(Profile, { }))
+  expect(component.client.subscriptions).toEqual([
+    { type: 'logux/subscribe', name: 'users/1', fields: ['photo'] }
+  ])
+
+  component.toJSON().props.onClick(2)
+  expect(component.client.subscriptions).toEqual([
+    { type: 'logux/subscribe', name: 'users/2', fields: ['photo'] }
+  ])
+})
+
+it('supports differnt store sources', function () {
+  var LoguxUserPhoto = subscribe(function (props) {
+    return 'users/' + props.id
+  }, {
+    storeKey: 'logux'
+  })(UserPhoto)
+
+  var createStore = createLoguxCreator({
+    subprotocol: '0.0.0',
+    server: 'wss://localhost:1337',
+    userId: false
+  })
+  var store = createStore(function () {
+    return { }
+  })
+
+  var Profile = createReactClass({
+    childContextTypes: {
+      logux: function () { }
+    },
+    getChildContext: function () {
+      return { logux: store }
+    },
+    render: function () {
+      return h('div', { onClick: this.change },
+        h(LoguxUserPhoto, { id: 1 })
+      )
+    }
+  })
+
+  createComponent(h(Profile, { }))
+  expect(store.client.subscriptions).toEqual([
+    { type: 'logux/subscribe', name: 'users/1' }
+  ])
+})
