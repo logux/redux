365eb58daed4e626f57bbc0b10eabf06f144cdab
Eduard Aksamitov <e@edaks.co>
2020-08-10 22:19:06 +0300

    feat(useDispatch): init

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 let { createLoguxCreator } = require('./create-logux-creator')
 let { useSubscription } = require('./use-subscription')
+let { useDispatch } = require('./use-dispatch')
 let { subscribe } = require('./subscribe')
 
 module.exports = {
   createLoguxCreator,
   useSubscription,
+  useDispatch,
   subscribe
 }
